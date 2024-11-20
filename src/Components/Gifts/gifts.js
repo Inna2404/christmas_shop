f3c3a55a20fc0e70fcfb5acd93ee6f7865ec0079
Inna <shcipchik.inn@gmail.com>
2024-11-20 12:40:31 +0200

    test_ing_2

--- src/Components/Gifts/gifts.js
+++ src/Components/Gifts/gifts.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
           <div class="gift_block_wrap">
             <a class="gift_bock">
-              <img src="/img/gift_ball_1.png" alt="gift_ball_work" />
+              <img src="img/gift_ball_1.png" alt="gift_ball_work" />
               <div class="gift_bock_title">
                 <p id="for_work">For work</p>
                 <h3>Console.log Guru</h3>
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
         
 
             <a class="gift_bock">
-              <img src="/img/gift_ball_2.png" alt="gift_ball_health" />
+              <img src="./img/gift_ball_2.png" alt="gift_ball_health" />
               <div class="gift_bock_title">
                 <p id="for_health">For health</p>
                 <h3>Hydration Bot</h3>
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
             </a>
 
             <a class="gift_bock">
-              <img src="/img/gift_ball_3.png " alt="gift_ball_work" />
+              <img src="img/gift_ball_3.png " alt="gift_ball_work" />
               <div class="gift_bock_title">
                 <p id="for_work">For work</p>
                 <h3>Merge Master</h3>
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
             </a>
 
             <a class="gift_bock">
-              <img src="/img/gift_ball_4.png" alt="gift_ball_harmony" />
+              <img src="img/gift_ball_4.png" alt="gift_ball_harmony" />
               <div class="gift_bock_title">
                 <p id="for_harmony">For harmony</p>
                 <h3>Spontaneous Coding Philosopher</h3>
