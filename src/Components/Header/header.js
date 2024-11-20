4b2488595090526c0dee024d733c59e01de39e4b
Inna <shcipchik.inn@gmail.com>
2024-11-20 12:59:53 +0200

    test2

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -3,7 +3,7 @@ document.getElementById("header").innerHTML = `
   <nav class="header_navigation">
     <ul class="header_navigation__list">
       <li class="header_navigation__home">
-        <a href="/index.html">
+        <a href="index.html">
           <div></div>
           the gifts
         </a>
@@ -12,10 +12,10 @@ document.getElementById("header").innerHTML = `
         <a href="./src/Components/Gifts/gifts_link.html"> gifts </a>
       </li>
       <li class="header_navigation__item">
-        <a href="/index.html#about"> about </a>
+        <a href="index.html#about"> about </a>
       </li>
       <li class="header_navigation__item">
-        <a href="/index.html#gifts"> best </a>
+        <a href="index.html#gifts"> best </a>
       </li>
       <li class="header_navigation__item">
         <a href="index.html#footer">contacts</a>
