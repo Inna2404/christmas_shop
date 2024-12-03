b2f2cbf6343466abc20f0ccc99eac57a84b25ca4
Inna <shcipchik.inn@gmail.com>
2024-12-03 13:05:03 +0200

    updatet_modal

--- modal_gifts.js
+++ modal_gifts.js
@@ -640,6 +640,29 @@ const giftData = [
     ]
   }
 ];
+// // Отримуємо контейнер для модального вікна
+// const modalContainer = document.getElementById("giftModal");
+
+// // Вставляємо вміст модального вікна динамічно
+// modalContainer.innerHTML = `
+//   <div id="giftModalContent" style="display: none" class="modal" aria-hidden="true">
+//     <div class="modal_content">
+//       <span class="close" onclick="closeModal()">&times;</span>
+//       <img id="modalImage" alt="gift_ball_work" src="" />
+//       <div class="modal_text_block">
+//         <div class="modal_text">
+//           <h4 id="modalCategory" aria-label="Category will be displayed here"></h4>
+//           <h3 id="modalTitle" style="color: #181c29" aria-label="Title will be displayed here"></h3>
+//           <p id="modalDescription" style="color: #181c29; font-weight: 400" class="modal_p"></p>
+//         </div>
+//         <div class="superpowers_block">
+//           <h4>Adds superpowers to:</h4>
+//           <div class="superpowers" id="superpowersBlock"></div>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// `;
 
 function openModal(event, giftiId) {
   event.preventDefault(); // Запобігає переходу по посиланню
--- src/Components/Gifts/gifts.js
+++ src/Components/Gifts/gifts.js
@@ -7,8 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
           </div>
 
           <div class="gift_block_wrap">
-            <a class="gift_bock">
-              <img src="img/gift_ball_1.png" alt="gift_ball_work" />
+            <a class="gift_bock" onclick="openModal(event,1)">
+              <img src="img/gift_ball_1.png"  alt="gift_ball_work" />
               <div class="gift_bock_title">
                 <h5 class="for_work">For work</h5>
                 <h3>Console.log Guru</h3>
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
             </a>
         
 
-            <a class="gift_bock">
+            <a class="gift_bock" onclick="openModal(event,2)" >
               <img src="./img/gift_ball_2.png" alt="gift_ball_health" />
               <div class="gift_bock_title">
                 <h5 class="for_health">For health</h5>
@@ -24,16 +24,16 @@ document.addEventListener("DOMContentLoaded", function () {
               </div>
             </a>
 
-            <a class="gift_bock">
-              <img src="img/gift_ball_3.png " alt="gift_ball_work" />
+            <a class="gift_bock" onclick="openModal(event,1)">
+              <img src="img/gift_ball_3.png" alt="gift_ball_work" />
               <div class="gift_bock_title">
                 <h5 class="for_work">For work</h5>
                 <h3>Merge Master</h3>
               </div>
             </a>
 
-            <a class="gift_bock">
-              <img src="img/gift_ball_4.png" alt="gift_ball_harmony" />
+            <a class="gift_bock" onclick="openModal(event,4)">
+              <img src="img/gift_ball_4.png"  alt="gift_ball_harmony" />
               <div class="gift_bock_title">
                 <h5 class="for_harmony">For harmony</h5>
                 <h3>Spontaneous Coding Philosopher</h3>
