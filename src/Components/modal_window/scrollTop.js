b55af04fb8e9783468c13269b5771af392743422
Inna <shcipchik.inn@gmail.com>
2024-12-04 11:47:46 +0200

    upScroll

---
+++ src/Components/modal_window/scrollTop.js
@@ -0,0 +1,19 @@
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollTopBtn.classList.add("show");
+  } else {
+    scrollTopBtn.classList.remove("show");
+  }
+};
+
+scrollTopBtn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
